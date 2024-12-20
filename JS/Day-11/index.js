9d1d20eb429674c1610eea9f4e596d4eacb985ba
Adepusrikanth9666 <srikanthadepu.17@gmail.com>
2024-12-20 08:07:33 +0530

    Day-11 Prototype in Objects

---
+++ JS/Day-11/index.js
@@ -0,0 +1,34 @@
+console.log("Day-11 Js class");
+
+// const userInfo = {
+//   name: "smith",
+//   age: "30",
+//   city: "Hyderabad",
+// };
+
+// userInfo.address = "Madhapur";
+
+// console.log(userInfo);
+
+// constructorss
+let value = 100;
+function Person(firstName, lastName, age, city) {
+  this.fName = firstName;
+  this.lName = lastName;
+  this.age = age;
+  this.city = city;
+  let value = 20;
+}
+
+let getFullName = (fName, lname) => {
+  // let value = 40;
+  console.log(value);
+  return fName + lname;
+};
+
+Person.prototype.address = "Hi-Tech City";
+Person.prototype.getFullName = getFullName;
+
+const emp_1 = new Person("Jhon", "Doe", 30, "Up");
+console.log(emp_1.address);
+console.log(emp_1.getFullName("sri", "Adepu"));
